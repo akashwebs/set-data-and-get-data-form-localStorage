15d1385d04c72431a0742162943ce717b597883a
akashwebs <aksah.shil227@gmail.com>
2022-03-07 01:57:37 +0600

    set item in localstorage done

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ document.getElementById('send-button').addEventListener('click', () => {
     const priceInput = parseInt(getInputValue('input-price'));
     displayBookAndPrice(bookName, priceInput);
 
+    // get data with parse object from loaclsotre fn call
+    getItemFromLocalSt();
+
 });
 let count = 0;
 const displayBookAndPrice = (bookName, price) => {
@@ -21,7 +24,29 @@ const displayBookAndPrice = (bookName, price) => {
         <td><button onclick='rmvProduct(event)' class="btn btn-danger">Remove</button></td>
     `
     productList.appendChild(tr);
+    setDataToLocalStorage(bookName, price);
+
 }
 const rmvProduct = event => {
     event.target.parentNode.parentNode.remove()
+}
+
+const getItemFromLocalSt = () => {
+    const getDataFromLocal = localStorage.getItem('bookStore');
+    let bookStore;
+    if (!getDataFromLocal) {
+        bookStore = {};
+    } else {
+        bookStore = JSON.parse(getDataFromLocal);
+    }
+    return bookStore;
+
+}
+
+const setDataToLocalStorage = (name, price) => {
+    const getObjectFromLocal = getItemFromLocalSt();
+    getObjectFromLocal[name] = price;
+    const strinfyData = JSON.stringify(getObjectFromLocal);
+    localStorage.setItem('bookStore', strinfyData);
+
 }
